61b2facb555ddf8369e3ee99731e7c0130a701d0
Anastasia Shatokhina <shnatya@gmail.com>
2021-12-08 08:59:31 -0800

    commit tree 1c2816f6ea2e19c73ce1a7a0939fb2d04bdea262

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React, {useEffect, useState} from "react";
+
+function App(){
+    const [dog, setData] = useState("")
+    const [isLoaded, setLoaded] = useState(false)
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(data => {
+            setData(data)
+            setLoaded(true)})
+    }, [])
+
+    if(!isLoaded){
+        return <p>Loading...</p>
+    }
+    return (
+        <img src={dog.message} alt = "A Random Dog"/>
+    )
+}
+
+export default App;
